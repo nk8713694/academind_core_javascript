0109d4853fd27687d05455b971b9b78a255277fb
nk8713694 <nk8713694@gmail.com>
2021-05-17 15:36:54 +0530

    intro to callback function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -83,7 +83,7 @@ startGameBtn.addEventListener('click', () => {
 
 // not related to game
 
-const sumUp = (a, b, ...numbers) => {
+const sumUp = (resultHandler, ...numbers) => {
   const validateNumber = (number) => {
     return isNaN(number) ? 0 : number;
   };
@@ -92,7 +92,7 @@ const sumUp = (a, b, ...numbers) => {
   for (const num of numbers) {
     sum += validateNumber(num);
   }
-  return sum;
+  resultHandler(sum);
 };
 
 const subtractUp = function() {
@@ -101,8 +101,12 @@ const subtractUp = function() {
     sum -= num;
   }
   return sum;
-}
+};
 
-console.log(sumUp(1, 5, 'fdsa', -3, 6, 10));
-console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+const showResult = (result) => {
+  alert('The result after adding all numbers is: ' + result);
+};
+
+sumUp(showResult, 1, 5, 'fdsa', -3, 6, 10);
+sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88);
 console.log(subtractUp(1, 10, 15, 20));
